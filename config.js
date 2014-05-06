b836a269f6cbcaa52fef2f8bf842868acb7249ef
Nicholas Tassone <ntassone@gmail.com>
2014-05-06 19:30:14 -0300

    better config/gitignore

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://ouija.io',
         mail: {},
         database: {
             client: 'sqlite3',
